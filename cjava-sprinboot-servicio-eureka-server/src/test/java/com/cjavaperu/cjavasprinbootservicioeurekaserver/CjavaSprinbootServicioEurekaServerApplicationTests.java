8b7e86c76409904b2deac15edcd4b18f73a4451e
VeronicaGoCa <programandoenlinea@gmail.com>
2022-04-04 20:32:56 -0400

    microservicios

---
+++ cjava-sprinboot-servicio-eureka-server/src/test/java/com/cjavaperu/cjavasprinbootservicioeurekaserver/CjavaSprinbootServicioEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cjavaperu.cjavasprinbootservicioeurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CjavaSprinbootServicioEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ cjava-springboot-config-server/src/main/java/com/cjava/expert/CjavaSpringbootConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.cjava.expert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CjavaSpringbootConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CjavaSpringbootConfigServerApplication.class, args);
+	}
+
+}
